f53520efc0d77e0e80170b45acd9155cfabf2764
nicole <nicolewang196@163.com>
2021-08-20 16:40:17 +0800

    backup

--- src/test/java/com/bondsales/backend/UserTest.java
+++ src/test/java/com/bondsales/backend/UserTest.java
@@ -4,6 +4,7 @@ import com.bondsales.backend.controller.UserController;
 import com.bondsales.backend.dao.entity.User;
 import com.bondsales.backend.dao.mapper.UserMapper;
 import com.google.gson.Gson;
+import org.apache.ibatis.jdbc.Null;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -17,12 +18,12 @@ public class UserTest {
     @Test
     public void testInsert(){
         User user1=new User();
-        user1.setUserid(4L);
-        user1.setLogname("mulla196");
-        user1.setUsername("mulla196");
-        user1.setPassword("XUSHAN");
+        user1.setUserid(null);
+        user1.setLogname("diana96");
+        user1.setUsername("dianaHuaXin");
+        user1.setPassword("dfafdss");
+        System.out.println(userController.insert(user1));
 //        System.out.println(new Gson().toJson(user1));
-        userController.insert(user1);
 
     }
 
